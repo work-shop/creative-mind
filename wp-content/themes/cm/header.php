3bd775fd5f51b57249387969288c18c929366a24
K. Belardinelli <k@kangabell.co>
2015-04-23 14:52:42 -0400

    remove landing logo animation

--- wp-content/themes/cm/header.php
+++ wp-content/themes/cm/header.php
@@ -80,8 +80,6 @@
 
 		<?php get_template_part('partials/ie'); ?>
 
-		<?php if(is_home()): get_template_part('partials/landing_home'); else: get_template_part('partials/landing'); endif; ?>
-
 		<div id="wrapper" class="loading spy">
 
 			
