fee6807447a1f137ac7d12e333ee36cee1100670
K. Belardinelli <k@kangabell.co>
2015-04-24 10:38:32 -0400

    category header style

--- wp-content/themes/cm/partials/category_header.php
+++ wp-content/themes/cm/partials/category_header.php
@@ -3,10 +3,17 @@
 ?>
 <section id="category-header" class="block target padded jank bg-<?php echo $category->slug;?>">
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<h1 class=" serif m0 bold white <?php //echo $category->slug; ?> centered"><?php echo $category->name; ?></h1>
-				<h2 class="centered white <?php echo $category->name; ?>"><?php echo CM_Collection_Controller::get_category_description( $category->term_id ); ?></h2>
+		<div class="row mt2">
+			<div class="col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2 centered">
+				<h1 class=" m0 bold white <?php //echo $category->slug; ?>"><?php echo $category->name; ?></h1>
+				<p class="col-md-10 col-md-offset-1 white <?php echo $category->name; ?>"><?php echo CM_Collection_Controller::get_category_description( $category->term_id ); ?></p>
+				<ul class="col-md-12 mt2 white">
+					<li><a class="ml1 underline white">Brown IE + MBA Program Workshops</a></li>
+					<li><a class="ml1 underline white">Creative Mind Sponsored Projects</a></li>
+					<li><a class="ml1 underline white">Pick-me-up Projects</a></li>
+					<li><a class="ml1 underline white">Neuroscience Research</a></li>
+					<li><a class="ml1 underline white">Better World By Design</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
