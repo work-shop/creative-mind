a448787460e0e5eb8475a3cf3306e02116d12d37
Nic Schumann <nicolas_schumann@brown.edu>
2015-05-13 14:09:03 -0400

    refactored the category page with comments and includes

--- wp-content/themes/cm/category.php
+++ wp-content/themes/cm/category.php
@@ -1,9 +1,48 @@
-<?php get_header(); ?>
+<?php
+/**
+ * CATEGORY ( category.php ) 
+ *
+ * 1. Header
+ * 2. Category Header
+ * 3. Category Collections
+ * 4. Back + Share
+ * 5. Footer
+ * [ 6. Story Modal ]
+ *
+ */
+?>
 
-	<?php get_template_part('partials/category_header');?>	
+<?php 
 
-	<?php get_template_part('partials/collections');?>
+/** 1. Header */
+get_header(); 
 
-	<?php get_template_part('partials/site_description');?>			
+?>
 
-<?php get_footer(); ?>
+<?php 
+
+/** 2. Category Header */
+get_template_part('partials/category_header');
+
+?>	
+
+<?php 
+
+/** 3. Category Collections */
+get_template_part('partials/collections');
+
+?>
+
+<?php 
+
+/** 4. Back + Share */
+get_template_part('partials/back_and_share');
+
+?>	
+	
+<?php 
+
+/** 5. Footer */
+get_footer(); 
+
+?>
