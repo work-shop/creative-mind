1abbf9c68511290f826d7a1f0cd6f2c5d19d878e
Nic Schumann <nicolas_schumann@brown.edu>
2015-05-13 14:11:10 -0400

    updated page.php to reflect the proper include stucture

--- wp-content/themes/cm/page.php
+++ wp-content/themes/cm/page.php
@@ -1,7 +1,42 @@
-<?php get_header(); ?>
+<?php
+/**
+ * PAGE ( page.php ) 
+ *
+ * 1. Header
+ * 2. Page Content
+ * 4. Back + Share
+ * 5. Footer
+ * [ 6. Story Modal ]
+ *
+ */
+?>
 
-<?php get_template_part('partials/content_page');?>
 
-<?php get_template_part('partials/site_description');?>
+<?php 
 
-<?php get_footer(); ?>
+/** 1. Header */
+get_header(); 
+
+?>
+
+<?php 
+
+/** 2. Page Title, Page Introduction, Page Content */
+get_template_part('partials/content_page');
+
+?>
+
+
+<?php 
+
+/** 3. Back + Share */
+get_template_part('partials/back_and_share');
+
+?>
+
+<?php 
+
+/** 4. Footer */
+get_footer(); 
+
+?>
