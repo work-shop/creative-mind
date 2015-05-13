e74afc83f5b7320875a2ca5f931c43008d15c479
Nic Schumann <nicolas_schumann@brown.edu>
2015-05-13 14:09:18 -0400

    refactored the index page with comments and includes

--- wp-content/themes/cm/index.php
+++ wp-content/themes/cm/index.php
@@ -1,7 +1,49 @@
-<?php get_header(); ?>
+<?php
+/**
+ * HOME ( index.php ) 
+ *
+ * 1. Header
+ * 2. Site Description
+ * 3. Grid
+ * 4. Back + Share
+ * 5. Footer
+ * [ 6. Story Modal ]
+ *
+ */
+?>
 
-	<?php get_template_part('partials/site_description');?>	
 
-	<?php get_template_part('partials/slideshow');?>	
+<?php 
+
+/** 1. Header */
+get_header(); 
+
+?>
+
+<?php 
+
+/** 2. Site Description */
+get_template_part('partials/site_description');
+
+?>	
+
+<?php 
+
+/** 3. Grid */
+get_template_part('partials/grid');
+
+?>	
+
+<?php 
+
+/** 4. Back + Share */
+get_template_part('partials/back_and_share');
+
+?>	
 	
-<?php get_footer(); ?>
+<?php 
+
+/** 5. Footer */
+get_footer(); 
+
+?>
