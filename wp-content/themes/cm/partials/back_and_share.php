d75ff39c5d7a2b86ce3f23a5ffaa9d27155ed288
Greg Nemes <mail@gregnemes.com>
2015-05-13 15:15:45 -0400

    added back and share code snippets

--- wp-content/themes/cm/partials/back_and_share.php
+++ wp-content/themes/cm/partials/back_and_share.php
@@ -18,6 +18,34 @@
  */
 $category = CM_Collection_Controller::get_current_category();
 
-
 ?>
+
+
+<section class="block target share padded-less" id="share">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h1 class="bold centered mb1">
+				Share the Creativity!
+				</h1>
+			</div>
+			<div class="col-sm-4 col-sm-offset-4 mb3">
+				<div class="share-icons">
+					<a class="addthis_button_facebook"><span class="icon social" data-icon="F"></span></a>
+					<a class="addthis_button_twitter"><span class="icon social" data-icon="t"></span></a>
+					<a class="addthis_button_email"><span class="icon social" data-icon="m"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<a href="<?php echo esc_url( home_url( '/'.$category->slug ) ); ?>">
+	<h2 class="serif centered <?php echo $category->slug; ?>">
+		<span class="p1 border-<?php echo $category->slug; ?>">Back to <?php echo $category->name; ?></span>
+	</h2>
+	</a>
+
+
+</section>
 
