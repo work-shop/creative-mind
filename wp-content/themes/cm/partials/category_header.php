6707b952118e0bff3258448ba65769613a382c6d
Greg Nemes <mail@gregnemes.com>
2015-05-13 15:59:49 -0400

    updated category header with data

--- wp-content/themes/cm/partials/category_header.php
+++ wp-content/themes/cm/partials/category_header.php
@@ -1,24 +1,38 @@
+<?php
+/**
+ * CATEGORY > Category Header (category_header.php)
+ */
+?>
 
 <?php
 	/**
 	 * @var stdClass $category the current category we're in.
+	 * @var string $category_name the name of the current category
+	 * @var string $category_description the description of the current category
 	 */
 	$category = CM_Collection_Controller::get_current_category();
+	$category_name = $category->name;
+	$category_description = CM_Collection_Controller::get_category_description( $category->term_id );
 ?>
-<section id="category-header" class="block target padded jank bg-<?php echo $category->slug;?>">
-	<div class="container-fluid">
-		<div class="row mt2">
-			<div class="col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2 centered">
-				<h1 class=" m0 bold white <?php //echo $category->slug; ?>"><?php echo $category->name; ?></h1>
-				<h2 class="col-md-10 col-md-offset-1 white <?php echo $category->name; ?>"><?php echo CM_Collection_Controller::get_category_description( $category->term_id ); ?></h2>
-				<ul class="col-md-12 mt2 white">
-					<li><a class="ml1 underline bold white" href="#">Brown IE + MBA Program Workshops</a></li>
-					<li><a class="ml1 underline bold white" href="#">Creative Mind Sponsored Projects</a></li>
-					<li><a class="ml1 underline bold white" href="#">Pick-me-up Projects</a></li>
-					<li><a class="ml1 underline bold white" href="#">Neuroscience Research</a></li>
-					<li><a class="ml1 underline bold white" href="#">Better World By Design</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</section>
+
+<?php
+
+	/**
+	 * @var $collections array(array('title' => title, 'id' => id, 'permalink' => permalink )) the set of collections in this category.
+	 */
+	$collections = array_map( function( $p ) { return array(
+		'id' => $p->ID,
+		'title' => $p->post_title
+	); }, CM_Collection_Controller::get_collections_for_category( $category->term_id ) );
+
+	/**
+	 * Let's iterate through all the collections in teh category and construct a list.
+	 */
+	foreach ( $collections as $collection ) {
+		$collection_name = $collection[ 'title' ];
+		$collection_id = $collection['id'];
+		$collection_permalink = get_permalink( $collection_id );
+
+	}
+
+?>
--- wp-content/themes/cm/partials/site_description.php
+++ wp-content/themes/cm/partials/site_description.php
@@ -12,32 +12,7 @@
 				<h2 class="centered m1">
 				The <a href="<?php bloginfo('url' );?>/courses" class="border-bottom-courses">courses</a>, <a href="<?php bloginfo('url' );?>/research" class="border-bottom-research">research</a>, <a href="<?php bloginfo('url' );?>/interviews" class="border-bottom-interviews">interviews</a> and <a href="<?php bloginfo('url' );?>/lectures" class="border-bottom-lectures">lectures</a> developed by the Creative Mind Initiative explore the roots of creative thinking and dissolve disciplinary boundaries in search of new modes of teaching and learning.<br>
 				</h2>
-				<h3 class="centered hidden"><a href="<?php bloginfo('url' );?>">Learn More about the Creative Mind Initiative <span class="icon" data-icon="&#8222;"></span></a></h2>
 			</div>
 		</div>	
-	</div>
-</section>
-<section class="block target padded bg-white mb3" id="site-description">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<h2 class="centered bold brand m1">Explore the Creative Mind!</h2>
-			</div>	
-			<div class="col-xs-6 col-sm-3 col-md-3">	
-				<a href="<?php bloginfo('url' );?>/courses" class="full border-courses centered courses category-button"><h3 class="centered bold">Courses</h3></a>
-			</div>
-			<div class="col-xs-6 col-sm-3 col-md-3">	
-				<a href="<?php bloginfo('url' );?>/research" class="full border-research centered research category-button"><h3 class="centered bold">Research</h3></a>
-			</div>	
-			<div class="col-xs-6 col-sm-3 col-md-3">	
-				<a href="<?php bloginfo('url' );?>/interviews" class="full border-interviews centered interviews category-button"><h3 class="centered bold">Interviews</h3></a>
-			</div>	
-			<div class="col-xs-6 col-sm-3 col-md-3">	
-				<a href="<?php bloginfo('url' );?>/lectures" class="full border-lectures centered lectures category-button"><h3 class="centered bold">Lectures</h3></a>
-			</div>	
-			<div class="col-sm-4 col-sm-offset-4 mt2 hidden">	
-				<h3 class=""><a href="#collections" class=" jump bg-brand centered white category-button down large">Featured Collections <span class="icon" data-icon="ﬁ"></span></a></h3>
-			</div>									
-		</div>
 	</div>
 </section>
