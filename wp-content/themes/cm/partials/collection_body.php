ebfbe1cf7d04ba9aa18be8bbcfd30ea29b944565
K. Belardinelli <k@kangabell.co>
2015-05-18 19:10:46 -0400

    initial setup of slides for collection page

--- wp-content/themes/cm/partials/collection_body.php
+++ wp-content/themes/cm/partials/collection_body.php
@@ -31,7 +31,7 @@ $collection_name = $collection->post_title;
  * @var $stories array(WP_Post) an array of stories attached to this post.
  * @var int $story_count the number of stories in this collection
  */
-$stories = get_field('collection_stories', $collection->ID );
+$stories = CM_Collection_Controller::get_stories_for_collection( $collection_id );
 $story_count = count( $stories );
 
 /**
@@ -67,13 +67,46 @@ $start = count($stories_halves[0]) + 1;
 		</div>
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2">
-				<div class="slide border-<?php echo $category_nicename ?> padded-more clearfix">
-					<h3 class="text-center">Table of Contents</h3>
-					<?php 
-						echo CM_Collection_Controller::create_list( $stories_halves[0], 1, 'collection' ); 
-						echo CM_Collection_Controller::create_list( $stories_halves[1], $start, 'collection' ); 
-					?>
-				</div> <!-- end .slide -->
+				<div class="flexslider-story flexslider" id="story-gallery">
+					<ul class="slides clearfix">
+						<li class="slide border-<?php echo $category_nicename ?> padded-more clearfix">
+							<h3 class="text-center">Table of Contents</h3>
+							<?php 
+								echo CM_Collection_Controller::create_list( $stories_halves[0], 1, 'collection' ); 
+								echo CM_Collection_Controller::create_list( $stories_halves[1], $start, 'collection' ); 
+							?>
+						</li> <!-- end .slide -->
+						<?php foreach ($stories as $story) { 
+							$story_name = $story->post_title;
+							$story_description = $story->story_description;
+							$story_type = $story->story_media_type;
+							$story_permalink = get_permalink($story);
+						?>
+							<li class="slide border-<?php echo $category_nicename ?> text-center clearfix">
+							<a class="padded-more" href="<?php echo $story_permalink ?>">
+								<p><?php 
+									if ( $story_type == 'video' ) { echo "Watch Video"; } 
+									elseif ( $story_type == 'image_gallery' ) { echo "View Gallery"; } 
+								?></p>
+								<?php if ( $story_type == 'video' ) { ?>
+									<span class="icon" data-icon="&#210;"></span>
+								<?php } elseif ( $story_type == 'image_gallery' ) { ?>
+									<span class="icon" data-icon="&#8486;"></span>
+								<?php } ?>
+								<h3><?php echo $story_name ?></h3>
+								<p><?php echo $story_description ?></p>
+							</a>
+							</li>
+						 <?php } ?>
+					</ul>
+					<div class="flexslider-controls"></div> 
+					<div id="flex-previous-story" class="flexslider-direction flex-previous previous">
+						<span class="icon" data-icon="&#8250;"></span>
+					</div>					
+					<div id="flex-next-story" class="flexslider-direction flex-next next">
+						<span class="icon" data-icon="&#8249;"></span>
+					</div>								
+				</div> <!-- end .flexslider -->
 			</div>							
 		</div> <!-- end .row -->
 	</div> <!-- end .container -->
