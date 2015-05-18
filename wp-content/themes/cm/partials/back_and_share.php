eca8802311fde6b9f9159c7a58d465ee10fde5bb
K. Belardinelli <k@kangabell.co>
2015-05-18 15:30:51 -0400

    alignment, style tweaks for share and back links.

--- wp-content/themes/cm/partials/back_and_share.php
+++ wp-content/themes/cm/partials/back_and_share.php
@@ -25,66 +25,61 @@ $collection = get_post( get_the_ID() );
 
 ?>
 
-<footer>
-	<section class="block target share padded-less" id="share">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2">
-					<h1 class="bold centered mb1">
+<section class="block target padded-less" id="back-share">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div id="share-links">
+					<h3 class="bold centered mb1">
 					Share the Creativity!
-					</h1>
-				</div>
-				<div class="col-sm-4 col-sm-offset-4 mb3">
+					</h3>
 					<div class="share-icons">
 						<a class="addthis_button_facebook"><span class="icon social" data-icon="F"></span></a>
 						<a class="addthis_button_twitter"><span class="icon social" data-icon="t"></span></a>
 						<a class="addthis_button_email"><span class="icon social" data-icon="m"></span></a>
 					</div>
-				</div>
+				</div> <!-- end #share-links -->
+				<div id="back-link">
+					<?php if ( is_home() ) { ?>
+						<div class="h3 bold centered brand">
+						<a href="#">
+							<span class="icon-wrapper bg-brand"><span class="icon" data-icon="&#8218;"></span></span>
+							<span class="text">Back to Top</span>
+						</a>
+						</div>
+					<?php } elseif ( is_category() ) { ?>
+						<div class="h3 bold centered brand">
+						<a href="<?php echo esc_url( home_url() ); ?>">
+							<span class="icon-wrapper bg-brand"><span class="icon" data-icon="&#8218;"></span></span>
+							<span class="text">Home</span>
+						</a>
+						</div>
+					<?php } elseif ( is_singular('collections') ) { ?>
+						<div class="h3 bold centered <?php echo $category->slug; ?>">
+						<a href="<?php echo esc_url( home_url( '/'.$category->slug ) ); ?>">
+							<span class="icon-wrapper bg-<?php echo $category->slug; ?>"><span class="icon" data-icon="&#8218;"></span></span>
+							<span class="text">Back to <?php echo $category->name; ?></span>
+						</a>
+						</div>
+					<?php } else { ?>
+						<?php
+						/**
+						 * @var array(WP_Post) an array of collections that this story belongs to.
+						 */
+						$collection = CM_Story_Controller::get_collections_for_story( get_the_ID() )[0];
+						?>
+
+						<div class="h3 bold centered <?php echo $category->slug; ?>">
+						<a href="<?php echo get_permalink( $collection->ID ); ?>">
+							<span class="icon-wrapper bg-<?php echo $category->slug; ?>"><span class="icon" data-icon="&#8218;"></span></span>
+							<span class="text">Back to <?php echo $collection->post_title; ?></span>
+						</a>
+						</div>
+
+					<?php } ?>
+				</div> <!-- end #back-link" -->
 			</div>
 		</div>
-	</section>
-	<section id="back-link">
-		<?php if ( is_home() ) { ?>
-			<a href="#">
-			<div class="h2 centered brand">
-				<span class="icon-wrapper"><span class="icon" data-icon="&#8218;"></span></span>
-				<span class="p1">Back to Top</span>
-			</div>
-			</a>
-		<?php } elseif ( is_category() ) { ?>
-			<a href="<?php echo esc_url( home_url() ); ?>">
-			<div class="h2 centered brand">
-				<span class="icon-wrapper"><span class="icon" data-icon="&#8218;"></span></span>
-				<span class="p1">Home</span>
-			</div>
-			</a>
-		<?php } elseif ( is_singular('collections') ) { ?>
-			<a href="<?php echo esc_url( home_url( '/'.$category->slug ) ); ?>">
-			<div class="h2 centered <?php echo $category->slug; ?>">
-				<span class="icon-wrapper bg-<?php echo $category->slug; ?>"><span class="icon" data-icon="&#8218;"></span></span>
-				<span class="p1">Back to <?php echo $category->name; ?></span>
-			</div>
-			</a>
-		<?php } else { ?>
-			<?php
-			/**
-			 * @var array(WP_Post) an array of collections that this story belongs to.
-			 */
-			$collection = CM_Story_Controller::get_collections_for_story( get_the_ID() )[0];
-			?>
-
-			<a href="<?php echo get_permalink( $collection->ID ); ?>">
-			<div class="h2 centered <?php echo $category->slug; ?>">
-				<span class="icon-wrapper bg-<?php echo $category->slug; ?>"><span class="icon" data-icon="&#8218;"></span></span>
-				<span class="p1">Back to <?php echo $collection->post_title; ?></span>
-			</div>
-			</a>
-
-		<?php } ?>
-	</section>
-</footer>
-
-
-
+	</div> <!-- end .container-fluid -->
+</section>
 
