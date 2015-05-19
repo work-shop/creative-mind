4beac73dfed14fd9c2b231296ee755f1ba1614a4
K. Belardinelli <k@kangabell.co>
2015-05-19 11:24:10 -0400

    structure/alignment for meganav elements

--- wp-content/themes/cm/partials/mega_nav.php
+++ wp-content/themes/cm/partials/mega_nav.php
@@ -3,52 +3,116 @@
     <div class="modal-content">
       <section class="block">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="icon bold" data-icon="&#64258;" aria-hidden="true"></span></button>
-        <div class="container">
-          <div class="row m3">
-            <div class="col-md-3 col-sm-6">
-              <h2 class="courses bold underline mb1"><a href="<?php bloginfo('url'); ?>/courses">Courses</a></h2>
-              <ul>
-                <li><span class="icon bold courses" data-icon="&#8901;"></span><a href="#">Communicating Science Through Visual Media</a></li>
-                <li><span class="icon bold courses" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold courses" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-                <li><span class="icon bold courses" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold courses" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-              </ul>
+        <div class="container-fluid">
+
+          <div class="row">
+            <div class="col-sm-5 col-sm-offset-1 bold">
+              <h4><a href="<?php bloginfo('url'); ?>/about">About the Creative Mind</a></h4>
             </div>
-            <div class="col-md-3 col-sm-6">
-              <h2 class="research bold underline mb1"><a href="<?php bloginfo('url'); ?>/research">Research</a></h2>
-              <ul>
-                <li><span class="icon bold research" data-icon="&#8901;"></span><a href="#">Brown IE + MBA Program Workshops</a></li>
-                <li><span class="icon bold research" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold research" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-                <li><span class="icon bold research" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold research" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-              </ul>
-            </div>
-            <div class="col-md-3 col-sm-6">
-              <h2 class="interviews bold underline mb1"><a href="<?php bloginfo('url'); ?>/interviews">Interviews</a></h2>
-              <ul>
-                <li><span class="icon bold interviews" data-icon="&#8901;"></span><a href="#">Brown IE + MBA Program Workshops</a></li>
-                <li><span class="icon bold interviews" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold interviews" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-                <li><span class="icon bold interviews" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold interviews" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-              </ul>
-            </div>
-            <div class="col-md-3 col-sm-6">
-              <h2 class="lectures bold underline mb1"><a href="<?php bloginfo('url'); ?>/lectures">Lectures</a></h2>
-              <ul>
-                <li><span class="icon bold lectures" data-icon="&#8901;"></span><a href="#">Communicating Science Through Visual Media</a></li>
-                <li><span class="icon bold lectures" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold lectures" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-                <li><span class="icon bold lectures" data-icon="&#8901;"></span><a href="#">Some Other Workshop</a></li>
-                <li><span class="icon bold lectures" data-icon="&#8901;"></span><a href="#">Mysterious Scientific Workshop</a></li>
-              </ul>
+            <div class="col-sm-6 bold">
+              <h4><a href="<?php bloginfo('url'); ?>/contact">Contact Us</a></h4>
             </div>
           </div>
-          <div class="row m3 righted">
-            <h2 class="bold underline"><a href="<?php bloginfo('url'); ?>/about">About the Creative Mind</a></h2>
-          </div>
+          <div class="row white bg-courses-dark">
+            <div class="col-sm-1">
+              <header class="bold">
+                <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/courses">Courses</a></h4>
+              </header>
+            </div>
+            <div class="col-sm-11 bg-courses padded-less m0">
+              <div class="row">
+                <div class="col-sm-6 bold">
+                  <ul>
+                    <li><a href="#">Communicating Science Through Visual Media</a></li>
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6 bold">
+                  <ul> 
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+              </div> <!-- end .row -->
+            </div>
+          </div> <!-- end .row -->
+
+          <div class="row white bg-research-dark">
+            <div class="col-sm-1">
+              <header class="bold">
+                <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/research">Research</a></h4>
+              </header>
+            </div>
+            <div class="col-sm-11 bg-research padded-less m0 bold">
+              <div class="row">
+                <div class="col-sm-6 bold">
+                  <ul>
+                    <li><a href="#">Communicating Science Through Visual Media</a></li>
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6 bold">
+                  <ul> 
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+              </div> <!-- end .row -->
+            </div>
+          </div> <!-- end .row -->
+
+          <div class="row white bg-interviews-dark">
+            <div class="col-sm-1">
+              <header class="bold">
+                <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/interviews">Interviews</a></h4>
+              </header>
+            </div>
+            <div class="col-sm-11 bg-interviews padded-less m0">
+              <div class="row">
+                <div class="col-sm-6 bold">
+                  <ul>
+                    <li><a href="#">Communicating Science Through Visual Media</a></li>
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6 bold">
+                  <ul> 
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+              </div> <!-- end .row -->
+            </div>
+          </div> <!-- end .row -->
+
+          <div class="row white bg-lectures-dark">
+            <div class="col-sm-1">
+              <header class="bold">
+                <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/lectures">Lectures</a></h4>
+              </header>
+            </div>
+            <div class="col-sm-11 bg-lectures padded-less m0 bold">
+              <div class="row">
+                <div class="col-sm-6 bold">
+                  <ul>
+                    <li><a href="#">Communicating Science Through Visual Media</a></li>
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6 bold">
+                  <ul> 
+                    <li><a href="#">Some Other Workshop</a></li>
+                    <li><a href="#">Mysterious Scientific Workshop</a></li>
+                  </ul>
+                </div>
+              </div> <!-- end .row -->
+            </div>
+          </div> <!-- end .row -->
+
         </div> <!-- /.container -->
       </section>
     </div><!-- /.modal-content -->
