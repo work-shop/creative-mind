a9088e04d1e141b19c3505c9e0bc09e277ff96a4
K. Belardinelli <k@kangabell.co>
2015-05-21 09:57:04 -0400

    use create_list in meganav (doesn't pull in data from the right categories yet tho)

--- wp-content/themes/cm/partials/mega_nav.php
+++ wp-content/themes/cm/partials/mega_nav.php
@@ -1,3 +1,26 @@
+<?php
+  /**
+   * @var stdClass $category the current category we're in.
+   */
+  $category = CM_Collection_Controller::get_current_category();
+
+  /**
+   * @var $collections array(array('title' => title, 'id' => id, 'permalink' => permalink )) the set of collections in this category.
+   */
+  $collections = array_map( function( $p ) { return array(
+    'id' => $p->ID,
+    'title' => $p->post_title
+  ); }, CM_Collection_Controller::get_collections_for_category( $category->term_id ) );
+
+  /**
+   *
+   * @var array $collections_halves contains two arrays, each containing half the collections in a category
+   *
+  */
+  $collections_halves = CM_Collection_Controller::split_array( $collections );
+
+?>
+
 <div class="modal fade" id="megaNav" role="dialog" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -5,6 +28,7 @@
         <div class="container-fluid">
 
           <div class="row white bg-courses-dark">
+
             <div class="col-sm-1">
               <header class="bold">
                 <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/courses">Courses</a></h4>
@@ -12,49 +36,35 @@
             </div>
             <div class="col-sm-11 bg-courses padded-less m0">
               <div class="row">
-                <div class="col-sm-6 bold">
-                  <ul>
-                    <li><a href="#">Communicating Science Through Visual Media</a></li>
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-                <div class="col-sm-6 bold">
-                  <ul> 
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-              </div> <!-- end .row -->
+                <?php 
+                  echo CM_Collection_Controller::create_list( $collections_halves[0], null, 'category' ); 
+                  echo CM_Collection_Controller::create_list( $collections_halves[1], null, 'category' ); 
+                ?>
+              </div>
             </div>
+            
           </div> <!-- end .row -->
 
           <div class="row white bg-research-dark">
+
             <div class="col-sm-1">
               <header class="bold">
                 <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/research">Research</a></h4>
               </header>
             </div>
-            <div class="col-sm-11 bg-research padded-less m0 bold">
+            <div class="col-sm-11 bg-research padded-less m0">
               <div class="row">
-                <div class="col-sm-6 bold">
-                  <ul>
-                    <li><a href="#">Communicating Science Through Visual Media</a></li>
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-                <div class="col-sm-6 bold">
-                  <ul> 
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-              </div> <!-- end .row -->
+                <?php 
+                  echo CM_Collection_Controller::create_list( $collections_halves[0], null, 'category' ); 
+                  echo CM_Collection_Controller::create_list( $collections_halves[1], null, 'category' ); 
+                ?>
+              </div>
             </div>
+            
           </div> <!-- end .row -->
 
           <div class="row white bg-interviews-dark">
+
             <div class="col-sm-1">
               <header class="bold">
                 <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/interviews">Interviews</a></h4>
@@ -62,46 +72,31 @@
             </div>
             <div class="col-sm-11 bg-interviews padded-less m0">
               <div class="row">
-                <div class="col-sm-6 bold">
-                  <ul>
-                    <li><a href="#">Communicating Science Through Visual Media</a></li>
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-                <div class="col-sm-6 bold">
-                  <ul> 
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-              </div> <!-- end .row -->
+                <?php 
+                  echo CM_Collection_Controller::create_list( $collections_halves[0], null, 'category' ); 
+                  echo CM_Collection_Controller::create_list( $collections_halves[1], null, 'category' ); 
+                ?>
+              </div>
             </div>
+            
           </div> <!-- end .row -->
 
           <div class="row white bg-lectures-dark">
+
             <div class="col-sm-1">
               <header class="bold">
                 <h4 class="uppercase"><a href="<?php bloginfo('url'); ?>/lectures">Lectures</a></h4>
               </header>
             </div>
-            <div class="col-sm-11 bg-lectures padded-less m0 bold">
+            <div class="col-sm-11 bg-lectures padded-less m0">
               <div class="row">
-                <div class="col-sm-6 bold">
-                  <ul>
-                    <li><a href="#">Communicating Science Through Visual Media</a></li>
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-                <div class="col-sm-6 bold">
-                  <ul> 
-                    <li><a href="#">Some Other Workshop</a></li>
-                    <li><a href="#">Mysterious Scientific Workshop</a></li>
-                  </ul>
-                </div>
-              </div> <!-- end .row -->
+                <?php 
+                  echo CM_Collection_Controller::create_list( $collections_halves[0], null, 'category' ); 
+                  echo CM_Collection_Controller::create_list( $collections_halves[1], null, 'category' ); 
+                ?>
+              </div>
             </div>
+            
           </div> <!-- end .row -->
 
           <div class="row">
