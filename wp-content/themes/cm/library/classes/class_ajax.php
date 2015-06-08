bbf64a7b16bc0207f6f0e9367a1fda4b943bd641
Nic Schumann <nicolas_schumann@brown.edu>
2015-06-08 18:43:01 -0400

    php refactor / php handshake update

--- wp-content/themes/cm/library/classes/class_ajax.php
+++ wp-content/themes/cm/library/classes/class_ajax.php
@@ -20,47 +20,33 @@ class CM_Ajax {
 
 	public function get_story() {
 		global $post;
-		$error = '<h4 class="async error vertical-center bold brand">We couldn\'t get the collection!</h4>';
+
+		$error = 'We couldn\'t get the story! Sorry about that.';
 
 		if ( !isset($_POST['story']) ) {
 			die( json_encode( array(
-				'error'   => $error,
-			) ) );
-		}
-
-		if ( !isset($_POST['collection']) ) {
-			die( json_encode( array(
+				'success' => false,
 				'error'   => $error,
 			) ) );
 		}
 
 		$story = get_post( $_POST['story'] );
-		$collection = get_post( $_POST['collection'] );
-
-		$previous_story = CM_Story_Controller::get_previous_story_in_collection( $story->ID, $collection->ID );
-		$next_story = CM_Story_Controller::get_next_story_in_collection( $story->ID, $collection->ID );
 
 		$post = get_post( $story->ID );
 
 		setup_postdata( $post );
-		set_global('formatted_content', apply_filters('the_content', $post->post_content) );
-		set_global('collection', $collection );
 
 		ob_start();
 
 		get_template_part( 'partials/story' );
 
 		$html = ob_get_contents();
+		
 		ob_end_clean();
-
-		unset_global('collection');
-		unset_global('formatted_content');
 
 		die( json_encode( array(
 			'success' 			=> true,
-			'previous_story' 		=> array('title' => $previous_story->post_title, 'id' => $previous_story->ID ),
-			'story_title' 		=> $story->post_title,
-			'next_story'			=> array('title' => $next_story->post_title, 'id' =>  $next_story->ID),
+			'title' 		=> $story->post_title,
 			'post'   			=> $html
 		) ) );
 	}
